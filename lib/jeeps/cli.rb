a916ac04f8e37033471a957a701524f7043db1b4
hannansan91 <hnnahsandusky@gmail.com>
2020-07-01 19:51:51 -0400

    building out cli

--- lib/jeeps/cli.rb
+++ lib/jeeps/cli.rb
@@ -1,7 +1,7 @@
 # class Jeeps::CLI
 
 #     def welcome
-#         puts "Hey! Come to check out some Jeeps? You can use this CLI to check the mileage on the latest 4WD Wrangler Unlimited."
+#         puts "Hey! Come to check out some Jeeps? You can use this CLI to check the mileage on different year models of the 4WD Wrangler Unlimited."
 #         menu
 #     end
 
@@ -35,7 +35,7 @@
 #     end
 
 #     def exit
-#         puts "Don't forget the wave!"
+#         puts "Happy Trails! Don't forget the wave!"
 #     end
 
 #     def scrape_list
@@ -50,12 +50,21 @@
 class Jeeps::CLI
     def call
         puts "Thanks for coming to check out Wrangler mileage! Remember: it's the smiles per gallon, not the miles per gallon!"
+        get_available_years
+        get_user_year
+        # get_mpg_for(year)
+        # list_mpg
     end
 
+    def get_available_years
+        # to be scraped instead
+        @years = ['2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020']
+    end
 
-
-
-
-
-
+    def get_user_year
+        # list months
+        @years.each_with_index {|year, index|
+        puts "#{year} #{index}"
+    }
+    end
 end
