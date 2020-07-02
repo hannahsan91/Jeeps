0126e4172b1f316c8178da2132a3860b65bb0f6b
hannansan91 <hnnahsandusky@gmail.com>
2020-07-01 21:41:06 -0400

    building out year input info

--- lib/jeeps/cli.rb
+++ lib/jeeps/cli.rb
@@ -50,21 +50,33 @@
 class Jeeps::CLI
     def call
         puts "Thanks for coming to check out Wrangler mileage! Remember: it's the smiles per gallon, not the miles per gallon!"
-        get_available_years
+        get_years
+        list_years
         get_user_year
         # get_mpg_for(year)
         # list_mpg
     end
 
-    def get_available_years
+    def get_years
         # to be scraped instead
-        @years = ['2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020']
+        @years = ['2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
+    end
+
+    def list_years
+        puts "On what year are you interested in checking mileage?"
+        # list yeras
+        @years.each.with_index(1) do |year, index|
+            puts "#{index}. #{year}"
+        end
     end
 
     def get_user_year
-        # list months
-        @years.each_with_index {|year, index|
-        puts "#{year} #{index}"
-    }
+        chosen_year = gets.strip
+        # if valid_input(chosen_year, @years)
+        # end
+    end
+
+    def valid_input(input, data)
+        input.to_i <= data.length && input > 0
     end
 end
